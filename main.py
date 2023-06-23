e22fa7d16d43dc9a41f79dfc940e762d715b9bb5
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-23 15:23:49 +0530

    creating Form class main.py

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ import sqlite3
 from random import randint
 from email.message import EmailMessage
 import smtplib
+
+
+class Form:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Login Page")
+        self.root.geometry('600x400+400+90')
+        icon = PhotoImage(file="img1.png")
+        self.root.iconphoto(False, icon)
+        self.home()
+        self.root.mainloop()
+    
+
+
+
+
