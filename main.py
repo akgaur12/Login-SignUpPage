797a8e852cce054d5ff99abd222049080243223e
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-23 15:22:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import sqlite3
+from random import randint
+from email.message import EmailMessage
+import smtplib
